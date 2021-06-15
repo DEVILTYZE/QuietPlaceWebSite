fa597ae8760810f72af29909cf89a44bed206097
DEVILTYZE <brain12222@gmail.com>
2021-06-15 15:04:59 +0300

    Was created project.

---
+++ QuietPlaceWebProject/QuietPlaceWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuietPlaceWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
